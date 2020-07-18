c489830a1e119e69c513846c4012671cf7cbf1b5
m4dn355 <xm4dn355x@gmail.com>
2020-07-19 03:12:29 +0500

    Hello World!

--- Sandbox.py
+++ Sandbox.py
@@ -10,5 +10,26 @@
 #                                                                                                                   #
 #####################################################################################################################
 
+
 # TODO: Разобраться как устроен wxPython и попробовать нарисовать адаптивное окно.
 
+
+# First things, first. Import the wxPython package.
+import wx
+
+
+# Next, create an application object.
+app = wx.App()
+
+
+# Then a frame.
+frm = wx.Frame(None, title="Hello World")
+
+
+# Show it.
+frm.Show()
+
+
+if __name__ == '__main__':
+    # Start the event loop.
+    app.MainLoop()
